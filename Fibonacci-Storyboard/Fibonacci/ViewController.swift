3011f28f59f3728bdd19e818a36837ce77e85174
Thomas Schluchter <github@schluchter.eml.cc>
2014-08-23 11:03:34 -0700

    Rebuilt project with Storyboard

--- Fibonacci-Storyboard/Fibonacci/ViewController.swift
+++ Fibonacci-Storyboard/Fibonacci/ViewController.swift
@@ -10,62 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // Why is it necessary to declare variables holding UIView instances as optional? Is there any risk they could be nil?
-    var selectedValueLabel: UILabel?
-    var outputTextView: UITextView?
-    var theSlider : UISlider?
-    
-    let fibo = FibonacciModel()
-                            
-    override func viewDidLoad() {
+    //MARK: Lifecycle methods
+        override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.addASlider()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        selectedValueLabel!.text = String(Int(self.slider!.value))
     }
     
-    func addASlider() {
-        self.theSlider = UISlider(frame: CGRectMake(60, 244, 200, 20))
-        self.view.addSubview(self.theSlider!)
-        
-        self.theSlider!.minimumValue = 2
-        self.theSlider!.maximumValue = 50
-        
-        let minLabel = UILabel(frame: CGRectMake(70, 205, 40, 40))
-        minLabel.text = String(Int(self.theSlider!.minimumValue))
-        self.view.addSubview(minLabel)
-        
-        let maxLabel = UILabel(frame: CGRectMake(235, 205, 40, 40))
-        maxLabel.text = String(Int(self.theSlider!.maximumValue))
-        self.view.addSubview(maxLabel)
-        
-        self.selectedValueLabel = UILabel(frame: CGRectMake(140, 320, 40, 40))
-        self.selectedValueLabel!.text = String(Int(self.theSlider!.value))
-        self.view.addSubview(selectedValueLabel!)
-        
-        self.outputTextView = UITextView(frame: CGRectMake(20, 350, 300, 140))
-        self.view.addSubview(self.outputTextView!)
-        
-        self.theSlider!.addTarget(self, action: "sliderValueDidChange", forControlEvents: UIControlEvents.ValueChanged)
-
-    }
+    //MARK: Custom stuff
+    let fibo = FibonacciModel()
     
-    func sliderValueDidChange() {
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var selectedValueLabel: UILabel!
+    @IBOutlet var outputTextView: UITextView!
+    
+    @IBAction func sliderDidChange(sender: AnyObject) {
         var resultArray : [Int] = []
         var formattedOutputString = ""
         
-        self.selectedValueLabel!.text = String(Int(theSlider!.value))
+        self.selectedValueLabel!.text = String(Int(slider!.value))
         
-        resultArray = self.fibo.calculateFibonacciNumbers(upTo: Int(self.theSlider!.value))
-        formattedOutputString = "The Fibonacci sequence for \(Int(self.theSlider!.value)) is \(resultArray)"
+        resultArray = self.fibo.calculateFibonacciNumbers(upTo: Int(slider!.value))
+        formattedOutputString = "The Fibonacci sequence for \(Int(slider.value)) is \(resultArray)"
         
         self.outputTextView?.text = formattedOutputString
     }
-
-
 }
-
