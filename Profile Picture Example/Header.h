c7beff94f4968836d9bcfd6c99a7c136dd0c89d8
Thomas Schluchter <github@schluchter.eml.cc>
2014-09-06 16:16:01 -0700

    Fixed AFNetworking import via CocoaPods

---
+++ Profile Picture Example/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  Profile Picture Example
+//
+//  Created by Thomas Schluchter on 9/6/14.
+//  Copyright (c) 2014 Thomas Schluchter. All rights reserved.
+//
+
+#ifndef Profile_Picture_Example_Header_h
+#define Profile_Picture_Example_Header_h
+
+#import "AFNetworking.h"
+
+#endif
